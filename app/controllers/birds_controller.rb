bab9e3c2bf483b484d99409d00741fcb95e95f4b
Daisy Simiyu <d.daisysimiyu@gmail.com>
2022-09-28 22:20:56 +0300

    commit tree 1ffb38ce2efcbd840553899c661a5743dd54a411

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,6 +22,18 @@ class BirdsController < ApplicationController
     end
   end
 
+  def update 
+    bird = Bird.find_by(id: params[:id])
+    if bird 
+      bird.update(bird_params)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+  end
+  def bird_params
+    params.permit(:name, :species, :likes)
+  end
+
   private
 
   def bird_params
